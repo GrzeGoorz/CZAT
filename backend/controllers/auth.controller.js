4c0426577b512d1d604afa95e693163587e90ad8
Grze Gorz <grzegorz096@gmail.com>
2025-12-16 16:12:40 +0100

    4. Real time chat, socket IO

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,25 +1,27 @@
-import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import generateTokenAndCookie from "../utils/generateToken.js";
+import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
     const { fullName, username, password, confirmPassword, gender } = req.body;
+
     if (password !== confirmPassword) {
-      return res
-        .status(400)
-        .json({ message: "Password and Confirm Password do not match" });
-    }
-    const user = await User.findOne({ username });
-    if (user) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ error: "Passwords don't match" });
     }
 
-    //HASH PASSWORD
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    // HASH PASSWORD HERE
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    //AWATAR
+    // https://avatar-placeholder.iran.liara.run/
+
     const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
     const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
@@ -32,10 +34,10 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      // GENERATE JWT TOKEN
-
+      // Generate JWT token here
+      generateTokenAndSetCookie(newUser._id, res);
       await newUser.save();
-      generateTokenAndCookie(newUser._id, res);
+
       res.status(201).json({
         _id: newUser._id,
         fullName: newUser.fullName,
@@ -43,11 +45,11 @@ export const signup = async (req, res) => {
         profilePic: newUser.profilePic,
       });
     } else {
-      res.status(400).json({ message: "Invalid user data" });
+      res.status(400).json({ error: "Invalid user data" });
     }
   } catch (error) {
-    console.log("Error in signup conrtoller:", error.message);
-    res.status(500).json({ message: error.message });
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
@@ -59,12 +61,12 @@ export const login = async (req, res) => {
       password,
       user?.password || ""
     );
+
     if (!user || !isPasswordCorrect) {
-      return res.status(400).json({ message: "Invalid username or password" });
+      return res.status(400).json({ error: "Invalid username or password" });
     }
 
-    // GENERATE JWT TOKEN
-    generateTokenAndCookie(user._id, res);
+    generateTokenAndSetCookie(user._id, res);
 
     res.status(200).json({
       _id: user._id,
@@ -73,7 +75,7 @@ export const login = async (req, res) => {
       profilePic: user.profilePic,
     });
   } catch (error) {
-    console.log("Error in login conrtoller:", error.message);
+    console.log("Error in login controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
@@ -81,9 +83,9 @@ export const login = async (req, res) => {
 export const logout = (req, res) => {
   try {
     res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ message: "Logout successful" });
+    res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
-    console.log("Error in logout conrtoller:", error.message);
+    console.log("Error in logout controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,9 +1,18 @@
 import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 
 const LogoutButton = () => {
+  const { loading, logout } = useLogout();
   return (
     <div className="mt-auto">
-      <BiLogOut className="w-6 h-6 text-white cursor-pointer" />
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <div className="w-6 h-6 animate-spin rounded-full border-2 border-white border-t-transparent" />
+      )}
     </div>
   );
 };
